b160726d05b84af5c5fb066e565ad070b446cd79
MikroTik Admin <admin@mikrotik.local>
2026-01-28 01:03:26 +0800

    chore: cleanup unused debug and temporary files

--- apps/api/debug-require.cjs
+++
@@ -1,5 +0,0 @@
-const mod = require('node-routeros');
-console.log('Type:', typeof mod);
-console.log('Keys:', Object.keys(mod));
-console.log('Is Constructor?', typeof mod === 'function');
-if (mod.RouterOSClient) console.log('Has RouterOSClient');
