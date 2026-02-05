50286908f8886c30fec89ceb6d8ef010b79cdf6e
MikroTik Admin <admin@mikrotik.local>
2026-02-06 05:12:36 +0800

    fix(build): explictly export animation styles helpers from index

--- apps/web/src/components/map/index.js
+++ apps/web/src/components/map/index.js
@@ -2,6 +2,7 @@
 export { default as LineThicknessControl } from './LineThicknessControl';
 export { default as AnimatedPath } from './AnimatedPath';
 export { default as EditablePath } from './EditablePath';
+export { default as AntPath } from './AntPath';
 export { default as DeviceModal } from './DeviceModal';
 export { default as MapFAB } from './MapFAB';
 export { default as MapToolbar } from './MapToolbar';
@@ -13,3 +14,8 @@ export {
     createNetwatchIcon,
     createEditHandleIcon,
 } from './DeviceIcon';
+export {
+    default as ANIMATION_STYLES,
+    getAnimationStyle,
+    getAnimationStyleNames
+} from './animationStyles';
